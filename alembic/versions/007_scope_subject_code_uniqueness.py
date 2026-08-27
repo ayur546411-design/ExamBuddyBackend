defd0988c651279fd2a0c9841e5d6e1bc1f56ce7
ayur546411-design <ayur546411@gmail.com>
2026-08-27 19:10:08 +0530

    update

--- alembic/versions/007_scope_subject_code_uniqueness.py
+++ alembic/versions/007_scope_subject_code_uniqueness.py
@@ -7,7 +7,7 @@ from typing import Sequence, Union
 
 from alembic import op
 
-revision: str = '007_scope_subject_code_uniqueness'
+revision: str = '007_subject_code_scope'
 down_revision: Union[str, Sequence[str], None] = ('006_add_notifications_table', '3bb92709766e')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
